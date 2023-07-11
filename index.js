87abb7bd640ae5dbf696ef5621cd3a462d2d185e
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-07-11 18:34:39 +0300

    completed

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ function render(item) {
   const div = document.createElement("div")
   div.className = "comfy"
   div.innerHTML = `
-  <img src="${item.png}">`
+  <img class = "pfpimage" src="${item.png}"> `
   document.querySelector("#commentSide").appendChild(div)
+  const comment = document.createElement("p")
+  comment.innerText= item.content;
+  div.appendChild(comment)
+ 
+
 
 }
 function getting() {
@@ -12,3 +17,19 @@ fetch(' http://localhost:3000/comments')
 .then(comments => comments.forEach(item => render(item)))
 }
 getting()
+
+document.addEventListener("submit",(e) => {
+  e.preventDefault();
+  fetch(' http://localhost:3000/comments',{
+  method:"POST",
+  headers: {
+    'Content-Type': 'application/json'
+    },
+    body : JSON.stringify({
+      content: document.getElementById("newCommentInput").value,
+      png: 'https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=686&q=80'
+    })
+  })
+})
+
+  
